bbce62c2e1e41f8b2c6afc4095a92055dd1884f0
Davide Di Criscito <davide.dicriscito@gmail.com>
2022-09-12 22:05:48 +0200

    add VanillaSidebar

--- dist/bundle.js
+++ dist/bundle.js
@@ -1,1 +1,1 @@
-(()=>{"use strict";console.log("rootscss!")})();
+(()=>{"use strict";var r={460:()=>{}},e={};function t(i){var a=e[i];if(void 0!==a)return a.exports;var s=e[i]={exports:{}};return r[i](s,s.exports,t),s.exports}t(460),new VanillaSidebar({selector:".sidebar-right",triggerer:".sidebar-right-trigger",quitter:".sidebar-quitter",align:"right"}),new VanillaSidebar({selector:".sidebar-left",triggerer:".sidebar-left-trigger",quitter:".sidebar-quitter",align:"left"})})();
--- src/ts/index.ts
+++ src/ts/index.ts
@@ -1,1 +1,15 @@
-console.log("rootscss!");
+import "@ctrlmaniac/vanilla-sidebar";
+
+const sidebarRight = new VanillaSidebar({
+  selector: ".sidebar-right",
+  triggerer: ".sidebar-right-trigger",
+  quitter: ".sidebar-quitter",
+  align: "right",
+});
+
+const sidebarLeft = new VanillaSidebar({
+  selector: ".sidebar-left",
+  triggerer: ".sidebar-left-trigger",
+  quitter: ".sidebar-quitter",
+  align: "left",
+});
