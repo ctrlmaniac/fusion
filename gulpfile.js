6d8ea39753606c93f840dc3b331fcf2af1140b64
Davide Di Criscito <davide.dicriscito@gmail.com>
2022-09-12 18:11:39 +0200

    fix margins

--- gulpfile.js
+++ gulpfile.js
@@ -6,7 +6,7 @@ const sass = require("gulp-sass")(require("sass"));
 
 function buildStyles() {
   return gulp
-    .src("./src/**/*.scss")
+    .src("./src/scss/**/*.scss")
     .pipe(sass().on("error", sass.logError))
     .pipe(gulp.dest("./dist/css"));
 }
