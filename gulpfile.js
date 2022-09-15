b2975bb4c3a613cdf7d3f79df9b3b4ab57598b6c
Davide Di Criscito <davide.dicriscito@gmail.com>
2022-09-15 17:47:31 +0200

    add tables

--- gulpfile.js
+++ gulpfile.js
@@ -7,7 +7,7 @@ const sass = require("gulp-sass")(require("sass"));
 function buildStyles() {
   return gulp
     .src("./scss/**/*.scss")
-    .pipe(sass({ outputStyle: "compressed" }).on("error", sass.logError))
+    .pipe(sass().on("error", sass.logError))
     .pipe(gulp.dest("./dist"));
 }
 
